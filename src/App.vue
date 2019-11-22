6469564b1c2b2b371ff8379925f73c503856c311
youknowcast <ykc@daycrift.net>
2019-11-22 18:30:29 +0900

    トップ画面にピザ屋のイラストを追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img width=500px src=https://4.bp.blogspot.com/-3C4IvuQmu2U/VuKDqFcyugI/AAAAAAAA4vE/wxHz-wwYzmAn4j91H3c-PO8ZLgb2olsGA/s800/building_food_pizza.png />
     <h1>Welcome to AIIT pizza house</h1>
     <router-link to="/menu">Menu</router-link>
     <router-link to="/orders">Order List</router-link>
